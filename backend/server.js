c5ff2a2c494c615fb31ff95607b4641ae64fef03
Moxit Bansal <moxit@example.com>
2026-03-25 15:12:43 +0530

    Fix login to accept usernames as well as emails

--- backend/server.js
+++ backend/server.js
@@ -90,8 +90,8 @@ app.post('/api/login', async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Find user by email
-    const user = await User.findOne({ email });
+    // Find user by email or username
+    const user = await User.findOne({ $or: [{ email: email }, { username: email }] });
 
     if (!user) {
       return res.status(401).json({ 
